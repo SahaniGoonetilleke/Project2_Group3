fc0f52884a812f00da5a5ef2bc27b8bf9cfe223b
SahaniGoonetilleke <sahani236@gmail.com>
2017-03-28 01:00:20 +0530

    Warping, Delaunay triangulation

--- ImageMorphing/ImageMorphing/ImageMorphing/ImageMorphing.cpp
+++ ImageMorphing/ImageMorphing/ImageMorphing/ImageMorphing.cpp
@@ -16,7 +16,8 @@ using namespace std;
 
 //Global variables
 Mat monkey, man, morphed, dst;
-int alpha = 0; int const max_elem = 100;
+Mat morphedMan, morphedMonkey;
+int a = 0; int const max_elem = 100;
 string face_cascade_name = "../../haarcascades/haarcascade_frontalface_alt.xml";
 string eyes_cascade_name = "../../haarcascades/haarcascade_eye_tree_eyeglasses.xml";
 CascadeClassifier face_cascade;
@@ -27,7 +28,7 @@ vector<Point2f> monkeyPoints;
 // Linear blend/ Adding - simplest ;)
 void Blend(int, void*)
 {
-	double A = alpha / 100.0; double beta = (1.0 - A);
+	double A = a / 100.0; double beta = (1.0 - A);
 	addWeighted(monkey, A, man, beta, 0.0, dst);
 	imshow("Morphed", dst);
 }
@@ -40,7 +41,7 @@ void face(Mat image)
 	cvtColor(image, imageGray, COLOR_BGR2GRAY);
 	equalizeHist(imageGray, imageGray);			// Histogram equalization
 
-												// Detect faces
+	// Detect faces
 	face_cascade.detectMultiScale(imageGray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
 	// Set Region of Interest
 	cv::Rect roi_b;
@@ -75,6 +76,7 @@ void face(Mat image)
 	}
 	imshow("face", image);
 	waitKey(0);
+	destroyAllWindows();
 }
 
 // Mouse clicks
@@ -84,7 +86,7 @@ void MouseClick(int event, int x, int y, int flags, void* userdata)
 	if (event == EVENT_LBUTTONDOWN)
 	{
 		cout << x << " " << y << endl;
-		manPoints.push_back(Point(x, y));
+		manPoints.push_back(Point2f(x, y));
 		//vector<Point>* ptPtr = (vector<Point>*)userdata;
 		//ptPtr->push_back(cv::Point(x, y));
 		cout << "Mouse clicked position (" << x << " " << y << ")" << endl;
@@ -96,13 +98,216 @@ void MonkeyMouseClick(int event, int x, int y, int flags, void* userdata)
 	if (event == EVENT_LBUTTONDOWN)
 	{
 		cout << x << " " << y << endl;
-		monkeyPoints.push_back(Point(x, y));
+		monkeyPoints.push_back(Point2f(x, y));
 		cout << "Mouse clicked position (" << x << " " << y << ")" << endl;
 	}
 }
 
-// Warping
 
+
+
+// Warping
+//Triangulation
+void draw_subdivide(Mat &img, Subdiv2D& subdiv, Scalar delaunay_color)
+{
+	vector<Vec6f> triangleList;
+	subdiv.getTriangleList(triangleList);
+	vector<Point> pt(3);
+	for (size_t i = 0; i < triangleList.size(); ++i)
+	{
+		Vec6f t = triangleList[i];
+		pt[0] = Point(cvRound(t[0]), cvRound(t[1]));
+		pt[1] = Point(cvRound(t[2]), cvRound(t[3]));
+		pt[2] = Point(cvRound(t[4]), cvRound(t[5]));
+		line(img, pt[0], pt[1], delaunay_color, 1);
+		line(img, pt[1], pt[2], delaunay_color, 1);
+		line(img, pt[2], pt[0], delaunay_color, 1);
+	}
+	//destroyAllWindows();
+}
+
+
+
+
+static void draw_subdiv_point(Mat& img, Point2f fp, Scalar color)
+{
+	circle(img, fp, 3, color, FILLED, LINE_8, 0);
+}
+
+static void draw_subdiv(Mat& img, Subdiv2D& subdiv, Scalar delaunay_color)
+{
+#if 1
+	vector<Vec6f> triangleList;
+	subdiv.getTriangleList(triangleList);
+	vector<Point> pt(3);
+
+	for (size_t i = 0; i < triangleList.size(); i++)
+	{
+		Vec6f t = triangleList[i];
+		pt[0] = Point(cvRound(t[0]), cvRound(t[1]));
+		pt[1] = Point(cvRound(t[2]), cvRound(t[3]));
+		pt[2] = Point(cvRound(t[4]), cvRound(t[5]));
+		line(img, pt[0], pt[1], delaunay_color, 1, LINE_AA, 0);
+		line(img, pt[1], pt[2], delaunay_color, 1, LINE_AA, 0);
+		line(img, pt[2], pt[0], delaunay_color, 1, LINE_AA, 0);
+	}
+#else
+	vector<Vec4f> edgeList;
+	subdiv.getEdgeList(edgeList);
+	for (size_t i = 0; i < edgeList.size(); i++)
+	{
+		Vec4f e = edgeList[i];
+		Point pt0 = Point(cvRound(e[0]), cvRound(e[1]));
+		Point pt1 = Point(cvRound(e[2]), cvRound(e[3]));
+		line(img, pt0, pt1, delaunay_color, 1, LINE_AA, 0);
+	}
+#endif
+}
+
+static void locate_point(Mat& img, Subdiv2D& subdiv, Point2f fp, Scalar active_color)
+{
+	int e0 = 0, vertex = 0;
+
+	subdiv.locate(fp, e0, vertex);
+
+	if (e0 > 0)
+	{
+		int e = e0;
+		do
+		{
+			Point2f org, dst;
+			if (subdiv.edgeOrg(e, &org) > 0 && subdiv.edgeDst(e, &dst) > 0)
+				line(img, org, dst, active_color, 3, LINE_AA, 0);
+
+			e = subdiv.getEdge(e, Subdiv2D::NEXT_AROUND_LEFT);
+		} while (e != e0);
+	}
+
+	draw_subdiv_point(img, fp, active_color);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Apply affine transform calculated using srcTri and dstTri to src
+void applyAffineTransform(Mat &warpImage, Mat &src, vector<Point2f> &srcTri, vector<Point2f> &dstTri)
+{
+	// Given a pair of triangles, find the affine transform.
+	Mat warpMat = getAffineTransform(srcTri, dstTri);
+	// Apply the Affine Transform just found to the src image
+	warpAffine(src, warpImage, warpMat, warpImage.size(), INTER_LINEAR, BORDER_REFLECT_101);
+}
+
+// Warps and alpha blends triangular regions from img1 and img2 to img
+void morphTriangle(Mat &img1, Mat &img2, Mat &img, vector<Point2f> &t1, vector<Point2f> &t2, vector<Point2f> &t, double alpha)
+{
+	// Find bounding rectangle for each triangle
+	Rect r = boundingRect(t);
+	Rect r1 = boundingRect(t1);
+	Rect r2 = boundingRect(t2);
+	// Offset points by left top corner of the respective rectangles
+	vector<Point2f> t1Rect, t2Rect, tRect;
+	vector<Point> tRectInt;
+	for (int i = 0; i < 3; i++)
+	{
+		tRect.push_back(Point2f(t[i].x - r.x, t[i].y - r.y));
+		tRectInt.push_back(Point(t[i].x - r.x, t[i].y - r.y)); // for fillConvexPoly
+		t1Rect.push_back(Point2f(t1[i].x - r1.x, t1[i].y - r1.y));
+		t2Rect.push_back(Point2f(t2[i].x - r2.x, t2[i].y - r2.y));
+	}
+	// Get mask by filling triangle
+	Mat mask = Mat::zeros(r.height, r.width, CV_32FC3);
+	fillConvexPoly(mask, tRectInt, Scalar(1.0, 1.0, 1.0), 16, 0);
+	// Apply warpImage to small rectangular patches
+	Mat img1Rect, img2Rect;
+	img1(r1).copyTo(img1Rect);
+	img2(r2).copyTo(img2Rect);
+	Mat warpImage1 = Mat::zeros(r.height, r.width, img1Rect.type());
+	Mat warpImage2 = Mat::zeros(r.height, r.width, img2Rect.type());
+	applyAffineTransform(warpImage1, img1Rect, t1Rect, tRect);
+	applyAffineTransform(warpImage2, img2Rect, t2Rect, tRect);
+	// Alpha blend rectangular patches
+	Mat imgRect = (1.0 - alpha) * warpImage1 + alpha * warpImage2;
+	// Copy triangular region of the rectangular patch to the output image
+	multiply(imgRect, mask, imgRect);
+	multiply(img(r), Scalar(1.0, 1.0, 1.0) - mask, img(r));
+	img(r) = img(r) + imgRect;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Draw delaunay triangles
+static void draw_delaunay(Mat& img, Subdiv2D& subdiv, Scalar delaunay_color)
+{
+	vector<Vec6f> triangleList;
+	subdiv.getTriangleList(triangleList);
+	vector<Point> pt(3);
+	Size size = img.size();
+	Rect rect(0, 0, size.width, size.height);
+	for (size_t i = 0; i < triangleList.size(); i++)
+	{
+		Vec6f t = triangleList[i];
+		pt[0] = Point(cvRound(t[0]), cvRound(t[1]));
+		pt[1] = Point(cvRound(t[2]), cvRound(t[3]));
+		pt[2] = Point(cvRound(t[4]), cvRound(t[5]));
+		// Draw rectangles completely inside the image.
+		if (rect.contains(pt[0]) && rect.contains(pt[1]) && rect.contains(pt[2]))
+		{
+			line(img, pt[0], pt[1], delaunay_color, 1, CV_AA, 0);
+			line(img, pt[1], pt[2], delaunay_color, 1, CV_AA, 0);
+			line(img, pt[2], pt[0], delaunay_color, 1, CV_AA, 0);
+		}
+	}
+	//---------------
+	//save triangle coordinates to txt file
+	//ofstream fileTriMan("../../../Images/ManTri.txt");
+	//vector<Vec6f>::iterator t = triangleList.begin();
+	//for (int t = 0; t < triangleList.size(); ++t)
+	//{
+	//	fileTriMan << triangleList[t] << endl;
+	//}
+	//fileTriMan.close();
+	//------------------
+}
 
 // Main function
 int main(int argc, char** argv)
@@ -115,14 +320,18 @@ int main(int argc, char** argv)
 	monkey = imread("../../Images/monkey1.jpg");
 	man = imread("../../Images/man1.jpg");
 	
-	//convert Mat to float data type
-	//monkey.convertTo(monkey, CV_32F);
-	//man.convertTo(man, CV_32F);
-	//empty average image
-	//Mat imgMorph = Mat::zeros(img1.size(), CV_32FC3);
-
 	resize(monkey, monkey, man.size());// Resize monkey to be the same size as man
 	morphed = man.clone();
+	morphedMan = man.clone();
+	morphedMonkey = monkey.clone();
+
+	namedWindow("Morphed", 1);
+	imshow("Morphed", morphed);
+	createTrackbar("Alpha", "Morphed", &a, max_elem, Blend);
+	waitKey(0);
+
+	// Classifier
+	face(morphedMan); face(morphedMonkey);
 
 	namedWindow("Man", 1);
 	//set the callback function for mouse event
@@ -155,7 +364,7 @@ int main(int argc, char** argv)
 	cout << "Monkey Points\n" << monkeyPoints << endl;
 
 	vector<Point2f> points;
-	alpha = 0.5;
+	double alpha = 0.5;			//alpha controls the degree of morph
 	//compute weighted average point coordinates
 	for (int i = 0; i < manPoints.size(); i++)
 	{
@@ -166,17 +375,102 @@ int main(int argc, char** argv)
 	}
 	cout << "Weighted Points\n" << points << endl;
 
-	// Triangulation
+	//convert Mat to float data type
+	//monkey.convertTo(monkey, CV_32F);
+	//man.convertTo(man, CV_32F);
+	//empty average image
+	//Mat imgMorph = Mat::zeros(img1.size(), CV_32FC3);
+
+	//...........
+	Scalar delaunay_color(255, 255, 255), points_color(0, 0, 255); // Define colors for drawing.
+	bool animate = true;					// Turn on animation while drawing triangles
+	//............
+	
+	// Triangulation points *******
+	Rect rectMan(0, 0, man.cols, man.rows);	// Outer bounding box
+	Subdiv2D subdivMan(rectMan);			// Create initial subdivision
+	Point2f fpMan;							// Point holder						
+	
+	// Insert points into subdiv
+	//for (int ip = 0; ip < manPoints.size(); ip++)
+	for (vector<Point2f>::iterator ip = manPoints.begin(); ip != manPoints.end(); ip++)
+	{
+		//fpMan = manPoints[ip];			// 32f points
+		subdivMan.insert(*ip);
+		if (animate)
+		{
+			Mat img_copy = man.clone();
+			// Draw delaunay triangles
+			draw_subdiv(img_copy, subdivMan, delaunay_color);
+			imshow("Delaunay Triangulation", img_copy);
+			waitKey(0);
+		}
+	}
+	// Draw delaunay triangles
+	draw_subdivide(man, subdivMan, delaunay_color);
+
+	// Draw points
+	for (vector<Point2f>::iterator it = points.begin(); it != points.end(); it++)
+	{
+		circle(man, *it, 2, points_color, CV_FILLED, CV_AA, 0);
+	}
+
+	imshow("Delaunay Triangulation", man);
+	waitKey(0);
 
 
 
 
-	// Classifier
-//	face(man); face(monkey);
+
+
+
+	Rect rectMonkey(0, 0, monkey.cols, monkey.rows);	// Outer bounding box - Same as man
+	Subdiv2D subdivMonkey(rectMonkey);		// Create initial subdivision
+	Point2f fpMonkey;					// Point holder
+	for (int i = 0; i < monkeyPoints.size(); i++)
+	{
+		fpMonkey = monkeyPoints[i];		// 32f points
+		subdivMonkey.insert(fpMonkey);
+	}
+	vector<Vec6f> trianglesMonkey;
+	subdivMonkey.getTriangleList(trianglesMonkey);
+
+
+
+
+
+
+
+	//Read triangle indices
+	ifstream ifs("../../../Images/tri.txt");
+	double x, y, z;
+	while (ifs >> x >> y >> z)
+	{
+		// Triangles
+		vector<Point2f> t1, t2, t;
+		// Triangle corners for man
+		cout << manPoints[x] << endl;
+		t1.push_back(manPoints[x]);
+		t1.push_back(manPoints[y]);
+		t1.push_back(manPoints[z]);
+		// Triangle corners for monkey
+		t2.push_back(monkeyPoints[x]);
+		t2.push_back(monkeyPoints[y]);
+		t2.push_back(monkeyPoints[z]);
+		// Triangle corners for morphed image.
+		t.push_back(points[x]);
+		t.push_back(points[y]);
+		t.push_back(points[z]);
+		morphTriangle(man, monkey, morphed, t1, t2, t, alpha);
+	}
+
+	// Display Result
+	imshow("Morphed Face", morphed / 255.0);
+	waitKey(0);
 
 
 	namedWindow("Morphed", 1);
 	imshow("Morphed", morphed);
-	createTrackbar("Alpha", "Morphed", &alpha, max_elem, Blend);
+	createTrackbar("Alpha", "Morphed", &a, max_elem, Blend);
 	waitKey(0);
 }
